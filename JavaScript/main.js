efbef4af40139a25e8558b1adbdfe701ab0029b1
Dinesh2112 <dineshpandian1931@gmail.com>
2023-09-07 19:06:00 +0530

    just adding files

---
+++ JavaScript/main.js
@@ -0,0 +1,18 @@
+function abc(a){
+    if (a>22){
+        console.log("you can leaglly preganant a lady with her permission");
+    }
+    else if(a>18){
+        console.log("you will die a play boy");
+    }
+    else{
+        console.log("if you pregnant a lady you will go to jail");
+    }
+}
+ a1= abc(23);
+ console.log(a1);
+ 
+ function clicked(){
+    document.getElementById("demo").innerHTML="<b>Welcome</b>";
+   
+ }
